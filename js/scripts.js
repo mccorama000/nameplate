943febd4f2648aab43b54625912ed419a05ac521
Amanda McCormack <amccormack@eagles.ewu.edu>
2016-10-17 09:24:14 -0700

    Added masonry.js Plugin

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,12 @@ $('h1').lettering();
 
 //Make Backstretch Go!!
 $.backstretch("img/mountain.jpg");
+
+//Make Masonry Go!!
+ $('selector').masonry()
+ 
+ $('.grid').masonry({
+  // options
+  itemSelector: '.grid-item',
+  columnWidth: 100
+});
